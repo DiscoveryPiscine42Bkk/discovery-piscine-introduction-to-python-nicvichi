5cba14ccedb284f27b9340c9d37a16d41fed9a6b
Nichapa Vichitraka <nicvichi@z1t4c1.42bangkok.com>
2025-05-06 13:49:32 +0700

    cell01

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+print("Hey,what's your first name?")
+x = input("please enter your first name :> ")
+print("And your last name?")
+y = input("please enter yoour last name :> ")
+print("Well,pleased to meet you",x,y)
