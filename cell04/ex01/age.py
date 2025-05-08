b6d86671f00d92f06c302c591fabc4e40d74f284
Nichapa Vichitraka <nicvichi@z2t19c4.42bangkok.com>
2025-05-08 13:41:39 +0700

    cell04

---
+++ cell04/ex01/age.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+print("Please tell me your age :")
+x = input("")
+print("You are currently",x,"years old.")
+years = [10,20,30]
+for y in years:
+    print("In",y,"years, you'll be",y + " " + x,"years old.")
---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+print("Give me the first number :")
+x = int(input())
+print("Give me the second number :")
+y = int(input())
+print("Thank you!")
+addition = x + y
+subtraction = x - y
+division = x / y
+multiplication = x * y
+print("%d + %d = %d"%(x,y,addition))
+print("%d - %d = %d"%(x,y,subtraction))
+print("%d / %d = %d"%(x,y,division))
+print("%d * %d = %d"%(x,y,multiplication))
