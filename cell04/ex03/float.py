f9c497bd2b1668ed66f92444e18f8f213d355176
Nichapa Vichitraka <nicvichi@z2t19c4.42bangkok.com>
2025-05-08 15:29:26 +0700

    test

---
+++ cell04/ex03/float.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+print("Give me a number :")
+x = float(input())
+if int(x) == x:
+    print("This number is an integer.")
+else:
+    print("This number is a decimal.")
---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+print("Give me a number :")
+import math
+number = float(input())
+round_up = math.ceil(number)
+print(f"{round_up}")
