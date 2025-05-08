99733a5c57e80e57539f2328b8239769c5cbfeae
Nichapa Vichitraka <nicvichi@z2t19c4.42bangkok.com>
2025-05-08 14:58:24 +0700

    cell03

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 
+import sys
+if len(sys.argv) == 1:
+    for x in range(11):
+        output_str = f"Table de {x}:"
+        for y in  range(11):
+            res = x * y
+            output_str += f" {res}"
+        print(output_str)
+else:
+    print("none")
