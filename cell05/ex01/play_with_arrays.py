ca26046b4bd38342acaf250cd75c4a9a6e693091
Nichapa Vichitraka <nicvichi@z2t19c4.42bangkok.com>
2025-05-09 10:24:44 +0700

    cell05

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+array1 = -42
+array2 = -28
+array3 = -14
+array4 = 0
+array5 = 14
+array6 = 28
+array7 = 42
+Original_array = [-42,-28,-14,0,14,28,42];
+New_array = [];
+print("Original_array :",Original_array)
+for x in Original_array:
+    New_array.append(x + 2)
+print("New_array :",New_array)
