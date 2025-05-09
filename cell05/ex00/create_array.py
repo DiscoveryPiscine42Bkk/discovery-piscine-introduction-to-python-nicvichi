4c3f2dce8c8318015aedc28f4975522f0089c1cf
Nichapa Vichitraka <nicvichi@z2t19c4.42bangkok.com>
2025-05-09 09:55:59 +0700

    cell05

---
+++ cell05/ex00/create_array.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#2,8,9,48,8,22,-12,2
+number1 = 2
+number2 = 8
+number3 = 9
+number4 = 48
+number5 = 8
+number6 = 22
+number7 = -12
+number8 = 2
+
+
+
+number = [2,8,9,48,8,22,-12,2];
+print(number)
